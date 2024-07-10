e31317aa5f6ab941847b9dc865e9615dc60e721c
Rishabh Garg <crayzmayz100@gmail.com>
2024-07-09 17:43:44 -0700

    Added modal functionality to projects

--- static/js/projects.js
+++ static/js/projects.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $(".tilt-wrapper").tilt({
         maxTilt: 15,
         perspective: 1400,
@@ -9,16 +9,37 @@ $(document).ready(function() {
         scale: 1.04
     });
     // Go through each project and if the description is too long, add a "Read More" button
-    $(".card").each(function() {
+    $(".card").each(function () {
         let description = $(this).find(".card-content");
         if (description.height() > 50) {
             // Cut off the text
+            let title = $(this).find(".card-title").text();
             let text = description.text();
             let cutOff = text.substring(0, 200);
             description.text(cutOff + "...");
             // Add the "Read More" button
             // TODO: Create a modal for the full description
-            $(this).append("<button class='read-more'>Read More</button>");
+            $(this).append(`<button class='read-more' data-title="${encodeURIComponent(title)}" data-text="${encodeURIComponent(text)}">Read More</button>`);
         }
     });
 });
+$(document).on('click', '.read-more', function () {
+    let title = decodeURIComponent($(this).data('title'));
+    let text = decodeURIComponent($(this).data('text'));
+    createModal(title, text);
+});
+function createModal(title, text) {
+    // Create the modal
+    let modal = $("#modal");
+    modal.removeClass("is-hidden");
+    let modalContent = modal.find(".modal-content");
+    modalContent.find(".modal-title").text(title);
+    modalContent.find(".modal-text").text(text);
+}
+
+function closeModal() {
+    let modal = $("#modal");
+    modal.addClass("is-hidden");
+    modal.find(".modal-content .modal-title").text("");
+    modal.find(".modal-content .modal-text").text("");
+}
